bcbe926e9dd106e9f5ae7c04aab09bf661de1faf
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-06-05 11:39:58 -0400

    4
    4

--- file2.js
+++ file2.js
@@ -1,6 +1,17 @@
-i = 0;
-console.log("hello!!")
-while ( i < 10 ) {
+// how do we wrap up our code in a FUNCTION
 
-  console.log(i++);
+function a() {
+    var sum = 0;
+    for (i = 0; i < 10; i++) {
+        // instructions go here!
+        console.log("HELLO Happy Summer!!!" + i);
+        sum += i;
+        // sum = sum + i;
+        if (i == 5) {
+            break;
+        }
+    }
+    return sum;
 }
+
+console.log("sum is ", a());

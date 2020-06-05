0b3f9bfd8ca62b1e69d7e3d763c484e2e754ab93
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-06-05 10:51:20 -0400

    1
    1

---
+++ File1.js
@@ -0,0 +1,11 @@
+// REPORT on the SUM of all numbers from 1000 going down to 17
+// Count down by 11 each time
+// ADD UP all the numbers you step over as you are counting down from 1000, going down 11 each time, until you get to 17
+// Output the result
+
+//  for loop works by iterating over a set of instructions a certain specified number of times:
+
+for (i = 0; i < 10; i++) {
+    // instructions go here!
+    console.log("HELLO Happy Summer!!!" + i);
+}
